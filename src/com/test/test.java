0c5314ac67af9a0da2c343d21e46bef130da0bc0
smallfIshw <1583693359@qq.com>
2022-04-14 18:10:34 +0800

    Initial commit

---
+++ src/com/test/test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author zxy
+ * @date 2022/4/14 - 17:53
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        
+    }
+}
